c2f62654cb7ff179779b645f83e76194f889905e
Jacob Ford <j@cobford.com>
2022-07-11 14:49:50 -0400

    Fix redundant JSON

--- api/login.js
+++ api/login.js
@@ -8,5 +8,5 @@ export default async (req, res) => {
   // cache for 24 hours, refresh in BG up to 30 days
   res.setHeader('Cache-Control', `s-maxage=${1 * 24 * 60 * 60}, stale-while-revalidate=${30 * 24 * 60 * 60}`)
 
-  res.json(JSON.stringify(cookies))
+  res.json(cookies)
 }
