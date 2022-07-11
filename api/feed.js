0755d6a6f790090eb55c15d0e9f2405f3fb4f5f7
Jacob Ford <j@cobford.com>
2022-07-11 15:35:31 -0400

    Fetch (and return) substack posts

--- api/feed.js
+++ api/feed.js
@@ -1,17 +1,27 @@
 const axios = require('axios')
 
 export default async (req, res) => {
-  let substackSid = ''
+  let substackSidCookie = ''
 
   if (process.env.SUBSTACK_SID) {
     // prevent 429 errors while developing
-    substackSid = process.env.SUBSTACK_SID
+    substackSidCookie = process.env.SUBSTACK_SID
   } else {
     // use URL to take advantage of caching
     const loginCookies = await axios.get(`http://${process.env.VERCEL_URL}/api/login`)
     console.log('loginCookies', loginCookies)
-    substackSid = loginCookies.data.find(cookie => cookie.startsWith('substack.sid='))
+    substackSidCookie = loginCookies.data.find(cookie => cookie.startsWith('substack.sid='))
   }
 
-  res.json(substackSid)
+  const substackPosts = await axios.get(
+    // page 0 = latest 12 posts
+    'https://api.substack.com/api/v1/inbox/posts?page=0',
+    {
+      headers: {
+        Cookie: substackSidCookie
+      }
+    }
+  )
+
+  res.json(substackPosts)
 }
