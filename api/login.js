cc824b7888388d17b1cd65b2a4ee52c8c0586d04
Jacob Ford <j@cobford.com>
2022-07-11 14:30:08 -0400

    Cache cookies, return JSON

--- api/login.js
+++ api/login.js
@@ -4,5 +4,9 @@ export default async (req, res) => {
   const loginResponse = await getLoginResponse
   console.log(loginResponse)
   const cookies = loginResponse.headers['set-cookie']
-  res.end(JSON.stringify(cookies))
+
+  // cache for 24 hours, refresh in BG up to 30 days
+  res.setHeader('Cache-Control', `s-maxage=${1 * 24 * 60 * 60}, stale-while-revalidate=${30 * 24 * 60 * 60}`)
+
+  res.json(JSON.stringify(cookies))
 }
