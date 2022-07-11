2d7b6921fdab837bec931267175b2f612cdea8ee
Jacob Ford <j@cobford.com>
2022-07-11 14:57:00 -0400

    Pause SID requests, getting 429 errors

--- api/feed.js
+++ api/feed.js
@@ -1,10 +1,14 @@
 const axios = require('axios')
 
 export default async (req, res) => {
-  // use URL to take advantage of caching
-  const loginCookies = await axios.get(`${process.env.VERCEL_URL}/api/login`)
-
-  const substackSid = loginCookies.find(cookie => cookie.startsWith('substack.sid='))
+  if (process.env.SUBSTACK_SID) {
+    // prevent 429 errors while developing
+    const substackSid = process.env.SUBSTACK_SID
+  } else {
+    // use URL to take advantage of caching
+    const loginCookies = await axios.get(`${process.env.VERCEL_URL}/api/login`)
+    const substackSid = loginCookies.find(cookie => cookie.startsWith('substack.sid='))
+  }
 
   res.json(substackSid)
 }
