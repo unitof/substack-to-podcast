e3a5e57eee05976da79f9f5c95f312c3df2fb5f0
Jacob Ford <j@cobford.com>
2022-07-11 15:17:09 -0400

    Forgot how JS scoping works

--- api/feed.js
+++ api/feed.js
@@ -1,13 +1,16 @@
 const axios = require('axios')
 
 export default async (req, res) => {
+  let substackSid = ''
+
   if (process.env.SUBSTACK_SID) {
     // prevent 429 errors while developing
-    const substackSid = process.env.SUBSTACK_SID
+    substackSid = process.env.SUBSTACK_SID
   } else {
     // use URL to take advantage of caching
-    const loginCookies = await axios.get(`${process.env.VERCEL_URL}/api/login`)
-    const substackSid = loginCookies.find(cookie => cookie.startsWith('substack.sid='))
+    const loginCookies = await axios.get(`http://${process.env.VERCEL_URL}/api/login`)
+    console.log('loginCookies', loginCookies)
+    substackSid = loginCookies.data.find(cookie => cookie.startsWith('substack.sid='))
   }
 
   res.json(substackSid)
