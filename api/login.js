61f0bf883d406dbdbc4cf8b402cfb8d3801e5022
Jacob Ford <j@cobford.com>
2022-07-17 23:38:12 -0400

    Prettier error handling

--- api/login.js
+++ api/login.js
@@ -1,17 +1,18 @@
 import getLoginResponse from '../lib/ss-login'
 
 export default async (req, res) => {
-  const loginResponse = await getLoginResponse
-  .catch(loginError => {
+  try {
+    const loginResponse = await getLoginResponse
+
+    const cookies = loginResponse.headers['set-cookie']
+
+    // cache for 24 hours, refresh in BG up to 30 days
+    res.setHeader('Cache-Control', `s-maxage=${1 * 24 * 60 * 60}, stale-while-revalidate=${30 * 24 * 60 * 60}`)
+    res.json(cookies)
+  }
+
+  catch (loginError) {
+    console.error(loginError)
     res.json(loginError)
-    console.log(loginError)
-    return
-  })
-  // console.debug(loginResponse)
-  const cookies = loginResponse.headers['set-cookie']
-
-  // cache for 24 hours, refresh in BG up to 30 days
-  res.setHeader('Cache-Control', `s-maxage=${1 * 24 * 60 * 60}, stale-while-revalidate=${30 * 24 * 60 * 60}`)
-
-  res.json(cookies)
+  }
 }
